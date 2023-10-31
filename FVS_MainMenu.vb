6c2995cdc811986881aebdad663090cf545388b1
Ty Garber <tyler.garber@dfw.wa.gov>
2023-10-31 11:04:45 -0700

    this succesfully ran 1502 FRAM runs

--- FVS_MainMenu.vb
+++ FVS_MainMenu.vb
@@ -16,77 +16,77 @@ Public Class FVS_MainMenu
 
 
     Private Sub FVS_MainMenu_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-      ReadOldCmd = False
-      FramVersionLabel.Text = "Version " & FramVersion
-      FormHeight = 762
-      FormWidth = 832
-      If DevWidth > My.Computer.Screen.Bounds.Width Then
-         FormWidthScaler = DevWidth / My.Computer.Screen.Bounds.Width
-      Else
-         FormWidthScaler = 1
-      End If
-      'FormWidthScaler = 1280 / My.Computer.Screen.Bounds.Width
-      '- Check if Form fits within Screen Dimensions
-      If (FormHeight > My.Computer.Screen.Bounds.Height Or _
-          FormWidth > My.Computer.Screen.Bounds.Width) Then
-         Me.Height = FormHeight / (DevHeight / My.Computer.Screen.Bounds.Height)
-         Me.Width = FormWidth / (DevWidth / My.Computer.Screen.Bounds.Width)
-         If FVS_MainMenu_ReSize = False Then
-            Resize_Form(Me)
-            FVS_MainMenu_ReSize = True
-         End If
-      End If
-      BackFramSave = False
-   End Sub
-
-   Private Sub FVS_Exit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles FVS_Exit.Click
-      Dim Result As Integer
-      If ChangeAnyInput = True Or ChangeBackFram = True Or ChangeFishScalers = True Or _
-         ChangeNonRetention = True Or ChangePSCMaxER = True Or ChangeSizeLimit = True Or _
-         ChangeStockFishScaler = True Or ChangeStockRecruit = True Then
-         ChangeAnyInput = True
-         Result = MsgBox("Input Values have been Changed!" & vbCrLf & "Save Current Model Run ???", MsgBoxStyle.YesNo)
-         If Result = vbYes Then
-                Call SaveModelRunInputs()
-            
+        ReadOldCmd = False
+        FramVersionLabel.Text = "Version " & FramVersion
+        FormHeight = 762
+        FormWidth = 832
+        If DevWidth > My.Computer.Screen.Bounds.Width Then
+            FormWidthScaler = DevWidth / My.Computer.Screen.Bounds.Width
+        Else
+            FormWidthScaler = 1
+        End If
+        'FormWidthScaler = 1280 / My.Computer.Screen.Bounds.Width
+        '- Check if Form fits within Screen Dimensions
+        If (FormHeight > My.Computer.Screen.Bounds.Height Or
+            FormWidth > My.Computer.Screen.Bounds.Width) Then
+            Me.Height = FormHeight / (DevHeight / My.Computer.Screen.Bounds.Height)
+            Me.Width = FormWidth / (DevWidth / My.Computer.Screen.Bounds.Width)
+            If FVS_MainMenu_ReSize = False Then
+                Resize_Form(Me)
+                FVS_MainMenu_ReSize = True
             End If
-      End If
+        End If
+        BackFramSave = False
+    End Sub
+
+    Private Sub FVS_Exit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles FVS_Exit.Click
+        Dim Result As Integer
+        If ChangeAnyInput = True Or ChangeBackFram = True Or ChangeFishScalers = True Or
+           ChangeNonRetention = True Or ChangePSCMaxER = True Or ChangeSizeLimit = True Or
+           ChangeStockFishScaler = True Or ChangeStockRecruit = True Then
+            ChangeAnyInput = True
+            Result = MsgBox("Input Values have been Changed!" & vbCrLf & "Save Current Model Run ???", MsgBoxStyle.YesNo)
+            If Result = vbYes Then
+                Call SaveModelRunInputs()
+
+            End If
+        End If
 
         End
 
-   End Sub
+    End Sub
 
-   Private Sub OpenDB_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles OpenDB.Click
+    Private Sub OpenDB_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles OpenDB.Click
 
-      Dim OpenFVSdatabase As New OpenFileDialog()
-      'Dim FVSdatabaseBackupName As String
-      'Dim DBNameLen As Integer
-      Dim Result As Integer
+        Dim OpenFVSdatabase As New OpenFileDialog()
+        'Dim FVSdatabaseBackupName As String
+        'Dim DBNameLen As Integer
+        Dim Result As Integer
 
-      If ChangeAnyInput = True Or ChangeBackFram = True Or ChangeFishScalers = True Or _
-         ChangeNonRetention = True Or ChangePSCMaxER = True Or ChangeSizeLimit = True Or _
-         ChangeStockFishScaler = True Or ChangeStockRecruit = True Then
-         ChangeAnyInput = True
-         Result = MsgBox("Input Values have been Changed!" & vbCrLf & "Save Current Model Run ???", MsgBoxStyle.YesNo)
-         If Result = vbYes Then
-            Call SaveModelRunInputs()
-         End If
-      End If
+        If ChangeAnyInput = True Or ChangeBackFram = True Or ChangeFishScalers = True Or
+           ChangeNonRetention = True Or ChangePSCMaxER = True Or ChangeSizeLimit = True Or
+           ChangeStockFishScaler = True Or ChangeStockRecruit = True Then
+            ChangeAnyInput = True
+            Result = MsgBox("Input Values have been Changed!" & vbCrLf & "Save Current Model Run ???", MsgBoxStyle.YesNo)
+            If Result = vbYes Then
+                Call SaveModelRunInputs()
+            End If
+        End If
 
 TryDBAgain:
-      FVSdatabasename = ""
+        FVSdatabasename = ""
         OpenFVSdatabase.Filter = "DataBase Files (*.db)|*.db|All files (*.*)|*.*"
         OpenFVSdatabase.FilterIndex = 1
-      OpenFVSdatabase.RestoreDirectory = True
-      If OpenFVSdatabase.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-         Try
-            FVSdatabasename = OpenFVSdatabase.FileName
-            FVSshortname = My.Computer.FileSystem.GetFileInfo(FVSdatabasename).Name
-            FVSdatabasepath = My.Computer.FileSystem.GetFileInfo(FVSdatabasename).DirectoryName
-         Catch Ex As Exception
-            MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
-         End Try
-      End If
+        OpenFVSdatabase.RestoreDirectory = True
+        If OpenFVSdatabase.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                FVSdatabasename = OpenFVSdatabase.FileName
+                FVSshortname = My.Computer.FileSystem.GetFileInfo(FVSdatabasename).Name
+                FVSdatabasepath = My.Computer.FileSystem.GetFileInfo(FVSdatabasename).DirectoryName
+            Catch Ex As Exception
+                MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+            End Try
+        End If
         If InStr(FVSdatabasename, "NewTransferModelRun.db") > 0 Or
          InStr(FVSdatabasename, "ModelRunTransfer.db") > 0 Then
             MsgBox("You cannot use the NewTransferModelRun or ModelRunTransfer" & vbCrLf &
@@ -95,11 +95,11 @@ TryDBAgain:
             GoTo TryDBAgain
         End If
         If FVSdatabasename.Length > 50 Then
-         DatabaseNameLabel.Text = FVSshortname
-      Else
-         DatabaseNameLabel.Text = FVSdatabasename
-      End If
-      If FVSdatabasename = "" Then Exit Sub
+            DatabaseNameLabel.Text = FVSshortname
+        Else
+            DatabaseNameLabel.Text = FVSdatabasename
+        End If
+        If FVSdatabasename = "" Then Exit Sub
 
         ''- Auto Save Backup-Copy of Database File
         'Me.Cursor = Cursors.WaitCursor
@@ -115,53 +115,53 @@ TryDBAgain:
 
 
 
-      '==============================================================================================
-      '- (Pete 12/13) Code that checks for the existence of the Target Sublegal:Legal Ratio and 
-      '  and RunEncounterRateAdjustment table (SLRatio) 
-      '- needed to use external sublegals; works to make things functional retroactively
-      Dim sql As String       'SQL Query text string
+        '==============================================================================================
+        '- (Pete 12/13) Code that checks for the existence of the Target Sublegal:Legal Ratio and 
+        '  and RunEncounterRateAdjustment table (SLRatio) 
+        '- needed to use external sublegals; works to make things functional retroactively
+        Dim sql As String       'SQL Query text string
         Dim SQLiteAdabater As SQLite.SQLiteDataAdapter
 
         'First check the FRAM database for the SLRatio and RunEncounterRateAdjustment tables
         FramDB.Open()
-      Dim restrictions1(3) As String
-      Dim restrictions2(3) As String
-      Dim DoesTableExist1 As Boolean
-      Dim DoesTableExist2 As Boolean
-      restrictions1(2) = "SLRatio"
+        Dim restrictions1(3) As String
+        Dim restrictions2(3) As String
+        Dim DoesTableExist1 As Boolean
+        Dim DoesTableExist2 As Boolean
+        restrictions1(2) = "SLRatio"
 
         Dim dbTbl As DataTable = FramDB.GetSchema("Tables", restrictions1)
         If dbTbl.Rows.Count = 0 Then
-         'Table does not exist
-         DoesTableExist1 = False
-      Else
-         'Table exists
-         DoesTableExist1 = True
-      End If
+            'Table does not exist
+            DoesTableExist1 = False
+        Else
+            'Table exists
+            DoesTableExist1 = True
+        End If
 
-      dbTbl.Dispose()
-      FramDB.Close()
+        dbTbl.Dispose()
+        FramDB.Close()
 
-      'If SLRatio doesn't exist, create it.
-      If DoesTableExist1 = False Then
-         sql = "CREATE TABLE SLRatio (RunID INTEGER,FisheryID INTEGER,Age INTEGER,TimeStep INTEGER,TargetRatio DOUBLE, RunEncounterRateAdjustment DOUBLE, UpdateWhen DATETIME, UpdateBy VARCHAR(255))"
+        'If SLRatio doesn't exist, create it.
+        If DoesTableExist1 = False Then
+            sql = "CREATE TABLE SLRatio (RunID INTEGER,FisheryID INTEGER,Age INTEGER,TimeStep INTEGER,TargetRatio DOUBLE, RunEncounterRateAdjustment DOUBLE, UpdateWhen DATETIME, UpdateBy VARCHAR(255))"
             'Now connect to the database and make the table...
             'create a command
             Dim my_Command As New SQLite.SQLiteCommand(sql, FramDB)
             FramDB.Open()
-         'command execute
-         my_Command.ExecuteNonQuery()
-         FramDB.Close()
-      End If
+            'command execute
+            my_Command.ExecuteNonQuery()
+            FramDB.Close()
+        End If
 
-      '==============================================================================================
+        '==============================================================================================
 
-      '- Recordset (Model Run) Selection
-      RecordsetSelectionType = 1
-      FVS_ModelRunSelection.ShowDialog()
-      Me.Visible = True
-      Me.BringToFront()
-      RecordSetNameLabel.Text = RunIDNameSelect
+        '- Recordset (Model Run) Selection
+        RecordsetSelectionType = 1
+        FVS_ModelRunSelection.ShowDialog()
+        Me.Visible = True
+        Me.BringToFront()
+        RecordSetNameLabel.Text = RunIDNameSelect
         'If RunIDSelect = 0 Then
         ' FVSdatabasename = "" 'Disabled so batch run will work - Ty
         'MsgBox("NO Recordsets Available in this Database File" & vbCrLf & _
@@ -300,7 +300,7 @@ TryDBAgain:
         Me.Label2.Text = "Model Run 1 of " & RunID.Length
 
         Dim startTime = DateTime.Now
-
+        MSFBiasFlag = True
         ' main loop
         For i = 0 To UBound(RunID)
             Me.Label2.Text = "Model Run " & i + 1 & " of " & RunID.Length
@@ -321,7 +321,7 @@ TryDBAgain:
                 Me.RecordSetNameLabel.Refresh()
             End If
 
-
+            Application.DoEvents()
         Next
     End Sub
 
